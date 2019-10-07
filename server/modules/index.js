901d38b6f5b36b0a686a520377c4246242b62728
tbaustin <tbaustin1992@gmail.com>
2019-10-07 11:03:27 -0500

    update modules

--- server/modules/index.js
+++ server/modules/index.js
@@ -1,10 +1,11 @@
 import { makeExecutableSchema } from 'apollo-server'
 import { mergeTypeDefs, mergeResolvers } from 'graphql-toolkit';
 
+const resolversPath = `${__dirname}/modules/**/resolvers/*.js`
+const typeDefsPaths = `${__dirname}/modules/**/typeDefs/*.gql`
 
 export const schema = makeExecutableSchema({
 	typeDefs: mergeTypeDefs(types),
 	resolvers: mergeResolvers(resolvers)
 })
 
-	`${__dirname}/modules/**/resolvers/*.js`
